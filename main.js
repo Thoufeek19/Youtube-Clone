872d6eee7f551e0e39e6f2abd6768258ca67dbbf
Thoufeek19 <thoufeek12604@gmail.com>
2025-09-20 18:41:35 +0530

    Youtube Clone

---
+++ main.js
@@ -0,0 +1,17 @@
+const themeToggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-theme");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    localStorage.setItem("darkMode", "enabled");
+  } else {
+    localStorage.setItem("darkMode", "disabled");
+  }
+});
